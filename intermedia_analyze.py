fc68f2c86a03f0bb044143bb8b108a2fc38101c7
Jie JW Wu <jiewu@gwu.edu>
2024-02-11 09:57:50 -0800

    add question quality; remove unused code
    add question quality; remove unused code

--- intermedia_analyze.py
+++ intermedia_analyze.py
@@ -1,12 +1,43 @@
 import argparse
 import json
 import os
+import openai
 import re
 import subprocess
 
 import nltk
 import pycode_similar
 from nltk.translate.bleu_score import sentence_bleu
+
+PROMPT_EVALUATE_QUESTIONS = 'Some information is removed from the original problem description. Questions are expected to get the information. Given the missing information, problem description, evaluate the quality of the questions. Return only an integer: 3 (Good), 2 (Fair), or 1 (Bad). ### Questions: {clarifying_questions} ### Problem Description: {problem} ### original description(includes missing information): {missing_information} \n'
+
+# TODO(jwu): adjust prompt
+def evaluate_clarifying_questions(
+    missing_information='',
+    clarifying_questions='',
+    problem=''
+):
+    topn = 1
+    temperature = 1.0
+    model = 'gpt-3.5-turbo'
+    completion = openai.ChatCompletion.create(
+        model=model,
+        n=topn,
+        temperature=temperature,
+        messages=[{
+            "role": "user",
+            "content": PROMPT_EVALUATE_QUESTIONS.format(
+                missing_information=missing_information,
+                clarifying_questions=clarifying_questions,
+                problem=problem
+            )
+        }]
+    )
+    response_list = []
+    for i in completion['choices']:
+        response_list.append(i['message']['content'])
+    # assume the result has only one element (n=1) which is only int
+    return ''.join(filter(str.isdigit, response_list[0]))
 
 # TODO(jwu): bug this code return last triple code snippet. 
 def response_2_code(response):
@@ -121,105 +152,6 @@ def solution_evaluation_HumanEval(solution, test_cases, demo_file, call_demo_fil
     print('%s/%s pass.' % (pass_num, len(test_cases)), flush=True)
     return passed_case, case_status
 
-def code_contest_analyze_process(log_file):
-    demo_file = 'demo.py'
-    problem_dic = {}
-    count = 0
-    while os.path.exists(demo_file):
-        demo_file = 'demo_%s.py' % count
-        count += 1
-    # initialize the record file
-    names = []
-    if not os.path.exists('./log/record/%s' % (log_file.split('/')[1])):
-        with open('./log/record/%s' % (log_file.split('/')[1]), 'w') as f:
-            f.write('')
-    else:
-        with open('./log/record/%s' % (log_file.split('/')[1]), 'r') as f:
-            for line in f.readlines():
-                content = json.loads(line)
-                names.append(content['name'])
-
-    with open('./tmp2/code_contests_test.json', 'r') as f:
-    # with open('dataset/code_contests_test.json', 'r') as f:
-        problem_list = json.load(f)
-        for i in range(len(problem_list)):
-            if not problem_list[i]['name'] in names:
-                pattern = re.compile(r'(?<=seconds:=)*\d+')
-                time_limit = pattern.findall(problem_list[i]['time_limit'].split('\n')[0])[0]
-                problem_dic[problem_list[i]['name']] = {
-                    'name': problem_list[i]['name'],
-                    'index_num': i,
-                    'time_limit': int(time_limit)
-                }
-
-    with open(log_file, 'r') as f:
-        for line in f.readlines():
-            content = json.loads(line)
-            name = content['name']
-            if name in names:
-                continue
-            index = content['index']
-            response = content['response']
-            if index == 0:
-                print('----------------------problem name: %s--------------------------------' % (name),
-                      flush=True)
-            # initialize
-            if 'code_candidates' not in problem_dic[name]:
-                problem_dic[name]['response_candidates'] = []
-                problem_dic[name]['code_candidates'] = []
-            print('generate code from response', flush=True)
-            # load from code_contest dataset
-            problem = problem_list[problem_dic[name]['index_num']]
-            test_set = problem['public_tests'] + problem['private_tests'] + problem['generated_tests']
-            reference_code = []
-            for candidate in problem['solutions']:
-                if candidate['language'] == 3:
-                    reference_code.append(candidate['solution'].split())
-
-            # get code from response
-            code = response_2_code(response)
-            # default weight: weights=(0.25, 0.25, 0.25, 0.25)
-            if reference_code == []:
-                BLEU_score_correct = -1
-            else:
-                BLEU_score_correct = sentence_bleu(reference_code, code.split())
-
-            # use code to run test cases
-            time_limit = problem_dic[name]['time_limit']
-            test_case_solved = solution_evaluation(code, test_set, demo_file, time_limit)
-            problem_dic[name]['response_candidates'].append(response)
-            res = {
-                'code': code,
-                'index': index,
-                'passed_case': test_case_solved[0],
-                'case_status': test_case_solved[1],
-                'BlEU_score_correct': BLEU_score_correct
-            }
-            problem_dic[name]['code_candidates'].append(res)
-
-            if index == 4:
-                print('%s stability analyze' % (name), flush=True)
-                code_candidates = []
-                code_reference = []
-                case_status_list = []
-                for code_res in problem_dic[name]['code_candidates']:
-                    code_candidates.append(code_res['code'].split())
-                    code_reference.append(code_res['code'])
-                    case_status_list.append(code_res['case_status'])
-                # semantic similarity
-                # semantic_similarity(problem_dic, name, code_candidates)
-                # syntatic similarity
-                # whether 5 output is same
-                # syntatic_similarity(problem_dic, name, code_candidates, case_status_list)
-                # structural_similarity
-                # structual_similarity(problem_dic, name, code_reference)
-                print('writing in %s' % (name), flush=True)
-                # write in
-                json_str = json.dumps(problem_dic[name])
-                with open('./log/record/%s' % (log_file.split('/')[1]), 'a') as f:
-                    f.write(json_str + '\n')
-                problem_dic.pop(name)
-
 def analyze_process_HumanEval(log_file, original_prompt_file):
     demo_file = 'demo.py'
     call_demo_file = 'call_demo.py'
@@ -275,6 +207,8 @@ def analyze_process_HumanEval(log_file, original_prompt_file):
                 continue
             index = content['index']
             response = content['response']
+            original_prompt = content['original_prompt']
+            modified_prompt = content['modified_prompt']
             if index == 0:
                 print('----------------------problem name: %s--------------------------------' % (name),
                       flush=True)
@@ -299,9 +233,12 @@ def analyze_process_HumanEval(log_file, original_prompt_file):
 
             # use code to run test cases
             time_limit = problem_dic[name]['time_limit']
+            question_quality_result = '0'
             if original_prompt_file != '' and code == '':
                 # response is asking questions. communication success. use original prompt results in this case
                 test_case_solved = [original_prompt_dic[name][index]['passed_case'], original_prompt_dic[name][index]['case_status']]
+                # evaluate clarifying questions
+                question_quality_result = evaluate_clarifying_questions(original_prompt,response,modified_prompt)
             else:
                 test_case_solved = solution_evaluation_HumanEval(code, test_set, demo_file, call_demo_file, problem['entry_point'], time_limit)
             res = {
@@ -309,27 +246,13 @@ def analyze_process_HumanEval(log_file, original_prompt_file):
                 'index': index,
                 'passed_case': test_case_solved[0],
                 'case_status': test_case_solved[1],
+                'question_quality': question_quality_result, 
                 # 'BlEU_score_correct': BLEU_score_correct
             }
             problem_dic[name]['response_candidates'].append(response)
             problem_dic[name]['code_candidates'].append(res)
             if index == 4:
                 print('%s stability analyze' % (name), flush=True)
-                # code_candidates = []
-                # code_reference = []
-                # case_status_list = []
-                # for code_res in problem_dic[name]['code_candidates']:
-                #     code_candidates.append(code_res['code'].split())
-                #     code_reference.append(code_res['code'])
-                #     case_status_list.append(code_res['case_status'])
-                # semantic similarity
-                # semantic_similarity(problem_dic, name, code_candidates)
-                # syntatic similarity
-                # whether 5 output is same
-                # syntatic_similarity(problem_dic, name, code_candidates, case_status_list)
-                # structural_similarity
-                # structual_similarity(problem_dic, name, code_reference)
-                # return problem_dic
                 print('writing in %s' % (name), flush=True)
                 # write in
                 json_str = json.dumps(problem_dic[name])
@@ -337,108 +260,9 @@ def analyze_process_HumanEval(log_file, original_prompt_file):
                     f.write(json_str + '\n')
                 problem_dic.pop(name)
 
-def analyze_process_APPS(log_file):
-    path = './APPS/test/'
-    demo_file = 'demo.py'
-    problem_dic = {}
-    count = 0
-    while os.path.exists(demo_file):
-        demo_file = 'demo_%s.py' % count
-        count += 1
-    # initialize the record file
-    names = []
-    if not os.path.exists('./log/record/%s' % (log_file.split('/')[1])):
-        with open('./log/record/%s' % (log_file.split('/')[1]), 'w') as f:
-            f.write('')
-    else:
-        with open('./log/record/%s' % (log_file.split('/')[1]), 'r') as f:
-            for line in f.readlines():
-                content = json.loads(line)
-                names.append(content['name'])
-
-    for dirpath, dirnames, filenames in os.walk(path):
-        # iterating for every problem
-        for i in range(len(dirnames[:500])):
-            if dirnames[:500][i] not in names:
-                problem_dic[dirnames[:500][i]] = {
-                    'name': dirnames[:500][i],
-                    'index_num': i,
-                    'time_limit': int(3)
-                }
-    with open(log_file, 'r') as f:
-        for line in f.readlines():
-            content = json.loads(line)
-            name = content['name']
-            if name in names:
-                continue
-            index = content['index']
-            response = content['response']
-            if index == 0:
-                print('----------------------problem name: %s--------------------------------' % (name),
-                      flush=True)
-            # initialize
-            if 'code_candidates' not in problem_dic[name]:
-                problem_dic[name]['response_candidates'] = []
-                problem_dic[name]['code_candidates'] = []
-            print('generate code from response', flush=True)
-            # load from code_contest dataset
-
-            with open(path + name + '/input_output.json', 'r', encoding='utf-8') as f:
-                test_case = json.load(f)
-            test_set = []
-            for i in range(len(test_case['inputs'])):
-                test_set.append({
-                    'input': test_case['inputs'][i],
-                    'output': test_case['outputs'][i]
-                })
-            reference_code = []
-            if os.path.exists(path + name + '/solutions.json',):
-                with open(path + name + '/solutions.json', 'r', encoding='utf-8') as f:
-                    solutions = json.load(f)
-            else:
-                solutions = []
-            for candidate in solutions:
-                reference_code.append(candidate.split())
-            code = response_2_code(response)
-            if reference_code == []:
-                BLEU_score_correct = -1
-            else:
-                BLEU_score_correct = sentence_bleu(reference_code, code.split())
-            time_limit = problem_dic[name]['time_limit']
-            test_case_solved = solution_evaluation(code, test_set, demo_file, time_limit)
-            problem_dic[name]['response_candidates'].append(response)
-            res = {
-                'code': code,
-                'index': index,
-                'passed_case': test_case_solved[0],
-                'case_status': test_case_solved[1],
-                'BlEU_score_correct': BLEU_score_correct
-            }
-            problem_dic[name]['code_candidates'].append(res)
-            if index == 4:
-                print('%s stability analyze' % (name), flush=True)
-                code_candidates = []
-                code_reference = []
-                case_status_list = []
-                for code_res in problem_dic[name]['code_candidates']:
-                    code_candidates.append(code_res['code'].split())
-                    code_reference.append(code_res['code'])
-                    case_status_list.append(code_res['case_status'])
-                # semantic similarity
-                # semantic_similarity(problem_dic, name, code_candidates)
-                # syntatic similarity
-                # whether 5 output is same
-                # syntatic_similarity(problem_dic, name, code_candidates, case_status_list)
-                # structural_similarity
-                # structual_similarity(problem_dic, name, code_reference)
-                # return problem_dic
-                print('writing in %s' % (name), flush=True)
-                # write in
-                json_str = json.dumps(problem_dic[name])
-                with open('./log/record/%s' % (log_file.split('/')[1]), 'a') as f:
-                    f.write(json_str + '\n')
-                problem_dic.pop(name)
-
+# extract code and run test cases
+# input: file in log/
+# output: file in log/record/
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
@@ -459,10 +283,5 @@ if __name__ == "__main__":
     )
 
     args = parser.parse_args()
-    if 'code_contest' in args.file:
-        code_contest_analyze_process(args.file)
-    elif 'HumanEval' in args.file:
+    if 'HumanEval' in args.file:
         analyze_process_HumanEval(args.file, args.originalFile)
-    elif 'APPS' in args.file:
-        analyze_process_APPS(args.file)
-    # problem_dic = analyze_process_APPS('log/dataset_APPS_model_gpt-3.5-turbo_topn_5_temperature_1.0.log_0')
