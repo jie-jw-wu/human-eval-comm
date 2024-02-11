36ce998c725dfeeabd3b17cdf24b8bc1fba728ff
Jie JW Wu <jiewu@gwu.edu>
2024-02-11 11:13:05 -0800

    nit comments
    nit comments

--- script_randRemove_30_temp_0_topn_5.bat
+++ script_randRemove_30_temp_0_topn_5.bat
@@ -1,6 +1,8 @@
 rem generate results of original prompt (4 examples).
 python3 generate_response.py -d HumanEval -m gpt-3.5-turbo -n 5 -t 0 -s 0 -o original
+rem extract code and run test cases for results.
 python3 intermedia_analyze.py -f log/dataset_HumanEval_model_gpt-3.5-turbo_topn_5_temperature_0.0.log_0
+rem compute test pass rate and comm. rate.
 python3 syntactic_similarity_OER.py  -e dataset_HumanEval  -m gpt-3.5-turbo -t 0 -o R1
 
 rem Now, continue to remove 30% content and generate results:
