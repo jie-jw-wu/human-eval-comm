9c358fa8b6cc03d54d757ed7ebffe2b3486b05e9
Jie JW Wu <jiewu@gwu.edu>
2024-02-14 13:16:49 -0800

    changes in scripts
    changes in scripts

--- scripts/script_manualRemove_30_temp_1_topn_5.bat
+++ scripts/script_manualRemove_30_temp_1_topn_5.bat
@@ -1,6 +1,6 @@
 rem Now, continue to remove 30% content and generate results:
 python3 generate_response.py -d HumanEvalComm -m gpt-3.5-turbo -n 1 -t 1 -s 0 -o manualRemove
-python3 intermedia_analyze.py -f log/manualRemove_30_dataset_HumanEvalComm_model_gpt-3.5-turbo_topn_5_temperature_1.0.log_0  
+python3 intermedia_analyze.py -f log/manualRemove_dataset_HumanEvalComm_model_gpt-3.5-turbo_topn_1_temperature_1.0.log_0 -of log/record/dataset_HumanEval_model_gpt-3.5-turbo_topn_5_temperature_1.0.log_0 -n 1
 
 rem Now, I will run syntactic_similarity_OER.py to get results.
 rem python3 syntactic_similarity_OER.py  -e manualRemove_30_dataset_HumanEvalComm  -m gpt-3.5-turbo -t 1 -o R1
