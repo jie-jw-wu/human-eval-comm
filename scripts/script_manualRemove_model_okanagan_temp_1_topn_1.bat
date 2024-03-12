0bd61f7b1527c38f63e0aa5af5e995c9f224ec7e
Jie JW Wu <jiewu@gwu.edu>
2024-03-11 17:50:23 -0700

    finish draft run tests of comm json

--- scripts/script_manualRemove_model_okanagan_temp_1_topn_1.bat
+++ scripts/script_manualRemove_model_okanagan_temp_1_topn_1.bat
@@ -1,5 +1,5 @@
 
-python generate_response.py -d HumanEvalComm -m Okanagan -n 1 -t 1 -s 0 -o manualRemove -maxp 1
+python generate_response.py -d HumanEvalComm -m Okanagan -n 1 -t 1 -s 0 -o manualRemove -maxp -1
 
 python intermedia_analyze.py -f log/manualRemove_dataset_HumanEvalComm_model_Okanagan_topn_1_temperature_1.0.log_0 -n 1
 
