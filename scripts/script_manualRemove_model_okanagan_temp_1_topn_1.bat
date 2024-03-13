24e9accb735a43d16e8c8f4f4b7490d0ae711e1b
Jie JW Wu <jiewu@gwu.edu>
2024-03-13 08:54:24 -0700

    display pass@k for okanagan

--- scripts/script_manualRemove_model_okanagan_temp_1_topn_1.bat
+++ scripts/script_manualRemove_model_okanagan_temp_1_topn_1.bat
@@ -3,6 +3,6 @@ python generate_response.py -d HumanEvalComm -m Okanagan -n 1 -t 1 -s 0 -o manua
 
 python intermedia_analyze.py -f log/manualRemove_dataset_HumanEvalComm_model_Okanagan_topn_1_temperature_1.0.log_0 -n 1
 
-rem python syntactic_similarity_OER.py -e manualRemove_dataset_HumanEvalComm -m gpt-3.5-turbo-0125 -t 1 -o R1 -n 1
+python syntactic_similarity_OER.py -e manualRemove_dataset_HumanEvalComm -m Okanagan -t 1 -o R1 -n 1
 
-rem python measurement_summary_draw_heatmap.py -e manualRemove -d HumanEvalComm -m gpt-3.5-turbo-0125 -t 1 -n 1
+python measurement_summary_draw_heatmap.py -e manualRemove -d HumanEvalComm -m Okanagan -t 1 -n 1
