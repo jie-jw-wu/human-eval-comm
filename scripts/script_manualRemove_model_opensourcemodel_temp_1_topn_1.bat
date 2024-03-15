49ef54fe59c78d82c00bb183d3d2b32dfac63dd7
Jie JW Wu <jiewu@gwu.edu>
2024-03-14 17:56:05 -0700

    fix typo

--- scripts/script_manualRemove_model_opensourcemodel_temp_1_topn_1.bat
+++ scripts/script_manualRemove_model_opensourcemodel_temp_1_topn_1.bat
@@ -1,6 +1,6 @@
 python generate_response.py -d HumanEvalComm -m starcoderbase-1b -n 1 -t 1 -s 0 -o manualRemove --hf_dir D:\Study\Research\Projects\huggingface --model_name_or_path bigcode/starcoderbase-1b -maxp -1
 
-rem for sockeye:  python generate_response.py -d HumanEvalComm -m starcoderbase-1b -ndir /scratch/st-fhendija-1/jwu/cache --model_name_or_path bigcode/starcoderbase-1b -maxp -1
+rem for sockeye:  python generate_response.py -d HumanEvalComm -m starcoderbase-1b -n 1 -t 1 -s 0 -o manualRemove --hf_dir /scratch/st-fhendija-1/jwu/cache --model_name_or_path bigcode/starcoderbase-1b -maxp -1
 
 
 rem python generate_response.py -d HumanEvalComm -m CodeLlama-7b-hf -n 1 -t 1 -s 0 -o manualRemove --hf_dir D:\Study\Research\Projects\huggingface --model_name_or_path codellama/CodeLlama-7b-hf -maxp -1
