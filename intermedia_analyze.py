79730ef55c093da6adc9635c2e2ab1362bfa632f
Nezar Zakaria <nezarmoustapha2003@gmail.com>
2025-10-15 15:56:28 -0700

    fix regex r to avoid syntax warning issue

--- intermedia_analyze.py
+++ intermedia_analyze.py
@@ -115,7 +115,7 @@ def solution_evaluation_HumanEval(solution, test_cases, demo_file, call_demo_fil
 
 # Return the first triple code snippet. 
 def response_2_code(response):
-    code_template = re.compile('```.*\n([\s\S]+?)\n```', re.M)
+    code_template = re.compile(r'```.*\n([\s\S]+?)\n```', re.M)
     code = code_template.findall(response)
     if len(code) > 0:
         return code[0] # code[-1] is the last triple code snippet
