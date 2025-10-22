9ec8ec7f63e7b8f2176def0bcf85ad80d2dfc933
Nezar Zakaria <nezarmoustapha2003@gmail.com>
2025-10-22 10:35:11 -0700

    revert dataset change

--- measurement_summary_draw_heatmap.py
+++ measurement_summary_draw_heatmap.py
@@ -74,7 +74,7 @@ def semantic_syntactic_structural_similarity(prompt_type):
             with open(file_path + '/%s_dataset_%s_%s_%s/intermediate_result_top0_5.json' % (experiment, dataset, model, temperature), 'r') as f:
                 intermediate_result = json.load(f)
     
-    with open(file_path + '/%s_dataset_%s_%s_%s/intermediate_result_among5.json' % (experiment, dataset, model, temperature), 'r') as fo:
+    with open(file_path + '/%s_dataset_%s_%s_%s/intermediate_result_among5.json' % (experiment, 'HumanEval', model, temperature), 'r') as fo:
         original_result = json.load(fo)
 
     test_case_pass_rate = []
